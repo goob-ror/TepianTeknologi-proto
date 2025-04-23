51517cf495f55f69dec65c1d49b9c9a6a3b32dca
SERPTO <scrow0ud@gmail.com>
2025-04-23 17:27:04 +0800

    fix for error commit

--- resources/js/honeycomb-background.js
+++ resources/js/honeycomb-background.js
@@ -1,41 +1,41 @@
-const patternElement = document.getElementById("pattern");
+// const patternElement = document.getElementById("pattern");
 
-const countY = Math.ceil(patternElement.clientHeight / 40) + 1;
-const countX = Math.ceil(patternElement.clientWidth / 48) + 1;
-const gap = 2;
+// const countY = Math.ceil(patternElement.clientHeight / 40) + 1;
+// const countX = Math.ceil(patternElement.clientWidth / 48) + 1;
+// const gap = 2;
 
-for (let i = 0; i < countY; i++) {
-  for (let j = 0; j < countX; j++) {
-    const hexagon = document.createElement("div");
-    hexagon.style = `
-      background: url('data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iODciIGhlaWdodD0iMTAwIiB2aWV3Qm94PSIwIDAgODcgMTAwIiBmaWxsPSJub25lIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPgo8cGF0aCBkPSJNMi4xOTg3MyAyNi4xNTQ3TDQzLjUgMi4zMDk0TDg0LjgwMTMgMjYuMTU0N1Y3My44NDUzTDQzLjUgOTcuNjkwNkwyLjE5ODczIDczLjg0NTNWMjYuMTU0N1oiIGZpbGw9IiMxMzEyMTciIHN0cm9rZT0iIzEzMTIxNyIgc3Ryb2tlLXdpZHRoPSI0Ii8+Cjwvc3ZnPgo=') no-repeat;
-      width: 44px;
-      height: 50px;
-      background-size: contain;
-      position: absolute;
-      top: ${i * 40}px;
-      left: ${j * 48 + ((i * 24) % 48)}px;
-    `;
+// for (let i = 0; i < countY; i++) {
+//   for (let j = 0; j < countX; j++) {
+//     const hexagon = document.createElement("div");
+//     hexagon.style = `
+//       background: url('data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iODciIGhlaWdodD0iMTAwIiB2aWV3Qm94PSIwIDAgODcgMTAwIiBmaWxsPSJub25lIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPgo8cGF0aCBkPSJNMi4xOTg3MyAyNi4xNTQ3TDQzLjUgMi4zMDk0TDg0LjgwMTMgMjYuMTU0N1Y3My44NDUzTDQzLjUgOTcuNjkwNkwyLjE5ODczIDczLjg0NTNWMjYuMTU0N1oiIGZpbGw9IiMxMzEyMTciIHN0cm9rZT0iIzEzMTIxNyIgc3Ryb2tlLXdpZHRoPSI0Ii8+Cjwvc3ZnPgo=') no-repeat;
+//       width: 44px;
+//       height: 50px;
+//       background-size: contain;
+//       position: absolute;
+//       top: ${i * 40}px;
+//       left: ${j * 48 + ((i * 24) % 48)}px;
+//     `;
 
-    patternElement.appendChild(hexagon);
-  }
-}
+//     patternElement.appendChild(hexagon);
+//   }
+// }
 
-let time = 0;
+// let time = 0;
 
-const loop = () => {
-  const gradientElement = document.getElementById("gradient");
+// const loop = () => {
+//   const gradientElement = document.getElementById("gradient");
   
-  time += 0.02;
-  const scale = 1 + 4 * Math.sin(time);
-  const opacity = 0.8 + 0.2 * Math.sin(time);
+//   time += 0.02;
+//   const scale = 1 + 4 * Math.sin(time);
+//   const opacity = 0.8 + 0.2 * Math.sin(time);
   
-  const centerX = window.innerWidth / 2;
-  const centerY = window.innerHeight / 2;
-  gradientElement.style.transform = `translate(${centerX}px, ${centerY}px) scale(${scale})`;
-  gradientElement.style.opacity = opacity;
+//   const centerX = window.innerWidth / 2;
+//   const centerY = window.innerHeight / 2;
+//   gradientElement.style.transform = `translate(${centerX}px, ${centerY}px) scale(${scale})`;
+//   gradientElement.style.opacity = opacity;
 
-  window.requestAnimationFrame(loop);
-};
+//   window.requestAnimationFrame(loop);
+// };
 
-window.requestAnimationFrame(loop);
+// window.requestAnimationFrame(loop);
--- resources/js/login-page.js
+++ resources/js/login-page.js
@@ -1,69 +1,69 @@
-function switchLoginForm() {
-    // Get the login and signin form elements by their IDs
-    const loginForm = document.getElementById('login');
-    const signinForm = document.getElementById('signin');
+// function switchLoginForm() {
+//     // Get the login and signin form elements by their IDs
+//     const loginForm = document.getElementById('login');
+//     const signinForm = document.getElementById('signin');
     
-    // Show the login form and hide the signin form
-    loginForm.classList.remove('hidden');
-    signinForm.classList.add('hidden');
-}
+//     // Show the login form and hide the signin form
+//     loginForm.classList.remove('hidden');
+//     signinForm.classList.add('hidden');
+// }
 
-function switchSigninForm() {
-    // Get the login and signin form elements by their IDs
-    const loginForm = document.getElementById('login');
-    const signinForm = document.getElementById('signin');
+// function switchSigninForm() {
+//     // Get the login and signin form elements by their IDs
+//     const loginForm = document.getElementById('login');
+//     const signinForm = document.getElementById('signin');
     
-    // Show the signin form and hide the login form
-    signinForm.classList.remove('hidden');
-    loginForm.classList.add('hidden');
-}
+//     // Show the signin form and hide the login form
+//     signinForm.classList.remove('hidden');
+//     loginForm.classList.add('hidden');
+// }
 
-document.addEventListener('DOMContentLoaded', function() {
-    const passwordToggles = document.querySelectorAll('.password-toggle');
-    passwordToggles.forEach(toggle => {
-        toggle.addEventListener('click', function() {
-            // Get the target input field
-            const targetId = this.getAttribute('data-target');
-            const passwordInput = document.getElementById(targetId);
+// document.addEventListener('DOMContentLoaded', function() {
+//     const passwordToggles = document.querySelectorAll('.password-toggle');
+//     passwordToggles.forEach(toggle => {
+//         toggle.addEventListener('click', function() {
+//             // Get the target input field
+//             const targetId = this.getAttribute('data-target');
+//             const passwordInput = document.getElementById(targetId);
             
-            // Toggle between password and text type
-            if (passwordInput.type == 'password') {
-                passwordInput.type = 'text';
-                // Change to "hide" icon
-                this.innerHTML = `
-                    <img src="assets/icons/crossed-eye.svg" alt="Toggle Password Visibility" class="h-5 w-5 text-gray-500">
-                `;
-            } else {
-                passwordInput.type = 'password';
-                // Change back to "show" icon
-                this.innerHTML = `
-                    <img src="assets/icons/eye.svg" alt="Toggle Password Visibility" class="h-5 w-5 text-gray-500">
-                `;
-            }
-        });
-    });
+//             // Toggle between password and text type
+//             if (passwordInput.type == 'password') {
+//                 passwordInput.type = 'text';
+//                 // Change to "hide" icon
+//                 this.innerHTML = `
+//                     <img src="assets/icons/crossed-eye.svg" alt="Toggle Password Visibility" class="h-5 w-5 text-gray-500">
+//                 `;
+//             } else {
+//                 passwordInput.type = 'password';
+//                 // Change back to "show" icon
+//                 this.innerHTML = `
+//                     <img src="assets/icons/eye.svg" alt="Toggle Password Visibility" class="h-5 w-5 text-gray-500">
+//                 `;
+//             }
+//         });
+//     });
 
-    const phoneInput = document.getElementById('phone');
-    if (phoneInput) {
-        phoneInput.addEventListener('input', function(e) {
-            // Remove all non-digit characters
-            let value = this.value.replace(/\D/g, '');
+//     const phoneInput = document.getElementById('phone');
+//     if (phoneInput) {
+//         phoneInput.addEventListener('input', function(e) {
+//             // Remove all non-digit characters
+//             let value = this.value.replace(/\D/g, '');
             
-            // Apply the phone number format
-            if (value.length > 0) {
-                if (value.length <= 3) {
-                    this.value = value;
-                } else if (value.length <= 7) {
-                    this.value = value.slice(0, 3) + '-' + value.slice(3);
-                } else {
-                    this.value = value.slice(0, 3) + '-' + value.slice(3, 7) + '-' + value.slice(7, 11);
-                }
-            }
+//             // Apply the phone number format
+//             if (value.length > 0) {
+//                 if (value.length <= 3) {
+//                     this.value = value;
+//                 } else if (value.length <= 7) {
+//                     this.value = value.slice(0, 3) + '-' + value.slice(3);
+//                 } else {
+//                     this.value = value.slice(0, 3) + '-' + value.slice(3, 7) + '-' + value.slice(7, 11);
+//                 }
+//             }
             
-            if (value.length > 12) {
-                this.value = this.value.substring(0, 15); // Account for 2 hyphens
-            }
-        });
-    }
+//             if (value.length > 12) {
+//                 this.value = this.value.substring(0, 15); // Account for 2 hyphens
+//             }
+//         });
+//     }
     
-});
+// });
